63b34bcf3c207dbc24d6fa8ee0762b21206b5492
SpiritChrusher <36460427+SpiritChrusher@users.noreply.github.com>
2020-05-01 20:07:24 +0200

    People have been created

---
+++ jpa-example/src/main/java/person/Main.java
@@ -0,0 +1,53 @@
+package person;
+
+import com.github.javafaker.Faker;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.time.ZoneId;
+import java.util.Locale;
+
+
+
+public class Main {
+
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private static Person newPerson() {
+        Person person = Person.builder()
+                .name(faker.name().fullName())
+                .dob(faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())
+                .gender(faker.options().option(Person.Gender.class))
+                .address(Address.builder()
+                        .country(faker.address().country())
+                        .state(faker.address().state())
+                        .city(faker.address().state())
+                        .streetAddress(faker.address().streetAddress())
+                        .zip(faker.address().zipCode())
+                        .build()
+                 )
+                .email(faker.internet().emailAddress())
+                .profession(faker.company().profession())
+                .build();
+        return person;
+    }
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-example");
+        EntityManager em = emf.createEntityManager();
+
+       int people = 39;
+       for (int i = 0; i < people; i++){
+           Person person = newPerson();
+           em.getTransaction().begin();
+           em.persist(person);
+           em.getTransaction().commit();
+           System.out.println(person);
+       }
+       em.close();
+       emf.close();
+
+    }
+
+}
